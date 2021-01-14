fd140c0533b9b8cc00a14701a54be3c8e1e6484a
Raynder Cardoso <raynder4@gmail.com>
2021-01-14 17:09:38 -0300

    subindo atualização de front-end e criando janela relatorios

--- front-end/script.js
+++ front-end/script.js
@@ -19,3 +19,79 @@ function limpar(){
     form = document.getElementById('formu');
     form.submit();
 }
+var tes = 0;
+function rt(){
+    console.log(tes);
+    tes = tes + 1;
+}
+function limpar(){
+    document.getElementById('cod').value = "";
+    form = document.getElementById('formu');
+    form.submit();
+}
+function abrirGerar(){
+    var cont = 2;
+    var te = document.getElementById('complet');
+    var te2 = document.getElementById('dec');
+    var te3 = document.getElementById('mes');
+    var wpx = 50;
+    var hpx = 20;
+
+    te.style.display = 'block';
+
+    setInterval(function (){
+        if(wpx < 300){
+            te.style.width = wpx+'px';
+            te.style.height = hpx+'px';
+
+            if(cont < 21){
+                te3.style.fontSize = cont+'px';
+                te2.style.fontSize = cont+'px';
+            }
+            wpx += 20;
+            hpx += 7;
+            if(cont > 9)
+                cont += 1;
+            if(cont > 11)
+                cont += 2;
+            else
+                cont += 1;
+
+        }
+    }, 15);    
+    
+}
+function criardoc(){
+    var nome = document.getElementById('nome').value;
+    var cpf = document.getElementById('cpf').value;
+    var cod = document.getElementById('cod').value;
+    var dec = document.getElementById('dec').value;
+    var mes = document.getElementById('mes').value;
+
+
+    var iFrame = document.createElement("iframe");
+    iFrame.addEventListener("load", function () { 
+    iFrame.contentWindow.focus();
+    iFrame.contentWindow.print();
+    window.setTimeout(function () {
+        document.body.removeChild(iFrame);
+    }, 0);
+    });        
+    iFrame.style.display = "none";
+    iFrame.src = "print.php?nome="+nome+"&cpf="+cpf+"&cod="+cod+"&dec="+dec+"&mes="+mes;
+    document.body.appendChild(iFrame);
+    }
+function direcDoc(){
+    var nome = document.getElementById('nome').value;
+    var cpf = document.getElementById('cpf').value;
+    var cod = document.getElementById('cod').value;
+    var dec = document.getElementById('dec').value;
+    var mes = document.getElementById('mes').value;
+
+
+    
+    window.location.href = "print.php?nome="+nome+"&cpf="+cpf+"&cod="+cod+"&dec="+dec+"&mes="+mes;
+}
+//Colocar um event para quando clicar no nome equivalente ao solicitado
+var clic = document.getElementById('ui-id-1');
+clic.addEventListener('click', rt);
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 ini_set( 'display_errors', 0 ); //Só usei isso, pois o array dados tem 22 posiçoes e usaria muita memoria pra fizer um try/catch para cada
 require_once "config.php";
-$buscas = new Buscas(); //A variavel tem que estão fora do escoplo dos If´s para funcionar em ambos os metodos.
 
 while($con < $cont){
     $dado[$con] = " ";
@@ -9,6 +8,7 @@ while($con < $cont){
 }
 
 if (isset($_POST['cnome']) || isset($_POST['cod'])){
+    $buscas = new Buscas(); //A variavel tem que estão fora do escoplo dos If´s para funcionar em ambos os metodos.
     if(!empty($_POST['cnome'])){
         $cnome = $_POST['cnome'];
         $dado = $buscas->byName($cnome);
@@ -71,24 +71,8 @@ if (isset($_POST['cnome']) || isset($_POST['cod'])){
         <input type="text" class="l w102" id="processo" value="<?php echo $dado[13];?>">
         <input type="text" class="info3" id="empenho" value="<?php echo $dado[14];?>">
         <input type="text" class="l w102" id="saldo" value="<?php echo $dado[15];?>">
-        <input type="text" class="i" id="inss" value="<?php if ($dado[16] == ''){
-            echo "";
-        }
-        elseif ($dado[16] == 1){
-         echo "contribuinte";
-        }
-        else{
-            echo "isento";
-        }?>">
-        <input type="text" class="i" id="iss" value="<?php if ($dado[18] == ''){
-            echo "";
-        }
-        elseif ($dado[18] == 1){
-         echo "contribuinte";
-        }
-        else{
-            echo "isento";
-        }?>">
+        <input type="text" class="i" id="inss" value="<?php if ($dado[16] == '') echo ""; elseif ($dado[16] == 1) echo "contribuinte"; else echo "isento"; ?>">
+        <input type="text" class="i" id="iss" value="<?php if ($dado[18] == '') echo ""; elseif ($dado[18] == 1) echo "contribuinte"; else echo "isento";?>">
 
         <input type="button" value="" class="hidden fin grav" onclick="gravar()">
         <input type="button" value="" class="hidden fin excl" onclick="excluir()">
@@ -96,114 +80,20 @@ if (isset($_POST['cnome']) || isset($_POST['cod'])){
         <input type="submit" value="" class="hidden">
         <input id="gerar" onclick="abrirGerar()" class="gerar fin" type="button" value="Gerar Declaração">
         <input id="conf" onclick="" class="fin conf" type="button" value="Avançado">
-        
-
-        
-
+  
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
-        
-
         <script>
-            
-            var tes = 0;
-            function rt(){
-                console.log(tes);
-                tes = tes + 1;
-            }
-            function limpar(){
-                document.getElementById('cod').value = "";
-                form = document.getElementById('formu');
-                form.submit();
-            }
-            $(function () {
-                $("#cnome").autocomplete({
-                    source: 'pesq.php'
-                });
+        $(function () {
+            $("#cnome").autocomplete({
+                source: 'pesq.php'
             });
-            function abrirGerar(){
-                var cont = 2;
-                var te = document.getElementById('complet');
-                var te2 = document.getElementById('dec');
-                var te3 = document.getElementById('mes');
-                var wpx = 50;
-                var hpx = 20;
-
-                te.style.display = 'block';
-
-                setInterval(function (){
-                    if(wpx < 300){
-                        te.style.width = wpx+'px';
-                        te.style.height = hpx+'px';
-
-                        if(cont < 21){
-                            te3.style.fontSize = cont+'px';
-                            te2.style.fontSize = cont+'px';
-                        }
-                        wpx += 20;
-                        hpx += 7;
-                        if(cont > 9)
-                            cont += 1;
-                        if(cont > 11)
-                            cont += 2;
-                        else
-                            cont += 1;
-
-                    }
-                }, 15);    
-                
-            }
-            function criardoc(){
-                var nome = document.getElementById('nome').value;
-                var cpf = document.getElementById('cpf').value;
-                var cod = document.getElementById('cod').value;
-                var dec = document.getElementById('dec').value;
-                var mes = document.getElementById('mes').value;
-
-
-                var iFrame = document.createElement("iframe");
-                iFrame.addEventListener("load", function () { 
-                iFrame.contentWindow.focus();
-                iFrame.contentWindow.print();
-                window.setTimeout(function () {
-                    document.body.removeChild(iFrame);
-                }, 0);
-                });        
-                iFrame.style.display = "none";
-                iFrame.src = "print.php?nome="+nome+"&cpf="+cpf+"&cod="+cod+"&dec="+dec+"&mes="+mes;
-                document.body.appendChild(iFrame);
-            }
-                //Colocar um event para quando clicar no nome equivalente ao solicitado
-                /*var clic = document.getElementById('ui-id-1');
-                clic.addEventListener('click', rt);*/
-            
-            
+        });
         </script>
 
         <style>
-            .gerarinp {
-                font-size: 2px;
-                text-align: center;
-                font-family: fangsong;
-                border-radius: 20px;
-                border-width: 0.01px;
-                outline: none;
-                margin-top: 4px;
-                }
-                .tnGerar {
-                    transform: translateX(-50%);
-                    background-color: #019CE5;
-                    color: #fff;
-                    margin-left: 50%;
-                    position: relative;
-                    padding: 7px;
-                    border-radius: 20px;
-                    }
-                .teste {
-                    border-radius: 30px;
-                    background-color: #d5e2f0;
-                }
+            
         </style>
         <img src="img/consulta.png">
     </div>
@@ -211,38 +101,29 @@ if (isset($_POST['cnome']) || isset($_POST['cod'])){
 
 <form id=complet class="complet" method="post" action="">
 
-
-
-
-
-
-
-<div style="" class="teste LoginContainer padding-3 ThemeGrid_Width5" id="wt10_wtMainContent_wtLoginContainer">
+    <div style="" class="teste LoginContainer padding-3 ThemeGrid_Width5" id="wt10_wtMainContent_wtLoginContainer">
     <div>
-        <div id="wt10_wtMainContent_wtFormLogin" class="OSAutoMarginTop">
-            <div align="center">
-                <p>Gerar Declaração</p>
-                <input class="gerarinp" name="dec" type="text" maxlength="50" tabindex="1" class="OSFillParent" placeholder="Informe o Codigo" id="dec" onkeydown="return OsEnterKey(&#39;wt10_wtMainContent_wt23&#39;, arguments[0] || window.event);" aria-invalid="false" style="margin-top: 0px;">
-                
-                <input type="text" name="cod" style="display: none;" value="<?php echo $cod; ?>">
-                <input type="text" name="cpf" style="display: none;" value="<?php echo $cpf; ?>">
-                <input type="text" name="nome" style="display: none;" value="<?php echo $nome; ?>">
-                
-                
-                <input name="mes" type="text" maxlength="20" id="mes" tabindex="2" class="gerarinp" placeholder="Informe o Mes">
-                    </span></div>
+            <div id="wt10_wtMainContent_wtFormLogin" class="OSAutoMarginTop">
+                <div align="center">
+                    <p>Gerar Declaração</p>
+                    <input class="gerarinp" name="dec" type="text" class="OSFillParent" placeholder="Informe o Codigo" id="dec" style="margin-top: 0px;">
                     
-                    <input class="btnGerar" onclick="criardoc()" type="button" name="wt10$wtMainContent$wt23" value="GERAR" id="wt10_wtMainContent_wt23" tabindex="3"  style="margin-left: 0px;background-color: #019CE5; color: #fff; transform: translateX(-50%);background-color: #019CE5;color: #fff;margin-left: 50%;position: relative;padding: 7px;border-radius: 20px;   top: 13px;">
-                    </div>
-                    </div>
-                    <div align="left" style="margin-top: 20px;">
-                        <div class="ThemeGrid_Width1">
-                            <span style="display: none;" class="ValidationMessage" role="alert" id="ValidationMessage_wt10_wtMainContent_wt8"></span></div><div class="ThemeGrid_Width10 ThemeGrid_MarginGutter"></div></div></div></div>
-
-    
-
-
-
+                    <input type="text" name="cod" style="display: none;" value="<?php echo $cod; ?>">
+                    <input type="text" name="cpf" style="display: none;" value="<?php echo $cpf; ?>">
+                    <input type="text" name="nome" style="display: none;" value="<?php echo $nome; ?>">
+                    
+                    <input name="mes" type="text" maxlength="20" id="mes" tabindex="2" class="gerarinp" placeholder="Informe o Mes">
+                </div>
+                        
+                <input class="btnGerar" onclick="criardoc()" type="button" name="wt10$wtMainContent$wt23" value="GERAR" id="wt10_wtMainContent_wt23">
+            </div>
+        </div>
+        <div align="left" style="margin-top: 20px;">
+            <div class="ThemeGrid_Width1">
+                <span style="display: none;" class="ValidationMessage" role="alert" id="ValidationMessage_wt10_wtMainContent_wt8"></span></div><div class="ThemeGrid_Width10 ThemeGrid_MarginGutter">
+            </div>
+        </div>
+    </div>   
 
 </form>
 
